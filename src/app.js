4e022e2f7b61958eddabebcabdfb22bac552eb24
JefersonPilligua <jefer.pilligua1509@gmail.com>
2024-09-18 16:37:58 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [" The dog", " My grandma", " The mailman", " My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let primero = Math.floor(Math.random() * who.length);
+  let segundo = Math.floor(Math.random() * action.length);
+  let tercero = Math.floor(Math.random() * what.length);
+  let cuarto = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[primero] + action[segundo] + what[tercero] + when[cuarto];
 };
